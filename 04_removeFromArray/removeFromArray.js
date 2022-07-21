18c8244826f1d6fb5f01ecc4d8f165882e1a07f8
amir-kedis <amir.kedis@gmail.com>
2022-07-21 13:32:48 +0200

    solve 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr = [], ...itemsToRemove) {
 
+    let result = [...arr];
+
+
+    for (itemToRemove of itemsToRemove) {
+
+        for (let i = 0;i<arr.length;i++) {
+
+            if (itemToRemove === arr[i]) {
+
+                result.splice(result.indexOf(itemToRemove),1);
+                
+            }
+        }
+
+    }
+
+    return result;
 };
 
 // Do not edit below this line
