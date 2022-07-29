fef34e0910dcb105b422f4fe4f6da6b7036ad485
amir-kedis <amir.kedis@gmail.com>
2022-07-29 14:35:11 +0200

    solve 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr = []) {
+	let sum = arr.reduce((total, current) => total + current, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr = []) {
+	let result = arr.reduce((total, current) => total * current, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let result = base;
+  for (let i = 1; i < power; i++) {
+    result *= base;
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
