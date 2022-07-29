82b839480e0e498aa3fdf8a121beb44c2a520175
amir-kedis <amir.kedis@gmail.com>
2022-07-29 14:44:53 +0200

    solve 09 plaindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const charsArr = Array.from(str.toLowerCase()).filter((char) => /^[a-zA-Z]+$/.test(char));
+    const charsStr = charsArr.join("");
+    const charsStrRev = charsArr.reverse().join("");
 
+    return charsStr === charsStrRev;
 };
 
 // Do not edit below this line
