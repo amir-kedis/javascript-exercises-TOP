e7a699f5ac606f0c22955ac925853b374d7afb9d
amir-kedis <amir.kedis@gmail.com>
2022-07-29 15:09:35 +0200

    solve 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people = []) {
 
+    const ordered = people.sort((a,b) => {
+        const currentYear = (new Date()).getFullYear();
+        let aAge, bAge;
+        if (!a.yearOfDeath) {
+            aAge = currentYear - a.yearOfBirth;
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (!b.yearOfDeath) {
+            bAge = currentYear - b.yearOfBirth;
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        return aAge > bAge ? -1 : 1;
+    });
+
+    return ordered[0];
 };
 
 // Do not edit below this line
